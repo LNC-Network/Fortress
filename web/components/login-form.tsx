b00b68e6307a97cefb9d0d15aa1883903f439020
Jit Debnath <jit.nathdeb@gmail.com>
2025-10-14 20:30:09 +0530

    changes

---
+++ web/components/login-form.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">User Name</FieldLabel>
+                <Input type="text" required />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                </div>
+                <Input type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit" className="bg-blue-800 hover:bg-blue-700">
+                  Login
+                </Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <a href="/signup">Sign up</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
